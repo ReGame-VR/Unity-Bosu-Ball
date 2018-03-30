dc9507ca78be57bafa788739e3163eb5f55f325a
ohughes343 <owen.hughes271828@gmail.com>
2018-03-30 11:18:03 -0400

    Update mpuwithteensy.c

--- mpuwithteensy.c
+++ mpuwithteensy.c
@@ -46,6 +46,7 @@
 #include "I2Cdev.h"
 #include <Firmata.h>
 #include "MPU6050_6Axis_MotionApps20.h"
+#include "Keyboard.h"
 #include "MPU6050.h" // not necessary if using MotionApps include file
 
 
